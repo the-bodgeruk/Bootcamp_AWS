c9a40bffbbfc9b350f8c6534e41c0bb315e14e9a
the-bodgeruk <walkertim@dsl.pipex.com>
2022-10-20 12:08:08 +0100

    updated to run server side

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,7 +1,21 @@
-const express =  require(`express`);
-const cors = require(`cors`);
+const express = require('express');
 const app = express();
-app.use(cors());
+
+// *** new path to the results file just to tidy a bit. Add path module to help create path to static
+const results = require('./data/results.json'); // Include some data to emulate a database
+const path = require('path'); // Used for concatenation to create a path
+// Point to static pages
+app.use(express.static(path.join(__dirname, '/public/html')));  // Client requests files relative to here - i.e. no path needed
+app.use(express.static(path.join(__dirname, '/public/img')));   // e.g. in index.html, <img src="http://localhost:3000/xx.jpg"> 
+app.use(express.static(path.join(__dirname, '/public/css')));  
+app.use(express.static(path.join(__dirname, '/public/js')));   
+
+// Home GET now returns the index.html instead of just a message. Note array for multiple paths
+app.get(['/','/index.html'], (req, res) => {
+  res.sendFile('./public/html/index.html', { root: __dirname }); // path is relative to the execution path __dirname
+});
+
+// *** 
 
 app.get(`/`, (req, res) => res.send(`We have an API!`));
 
